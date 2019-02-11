695532718b798027f7ef83ef7e33e157df24cd6f
ckjohn000 <ckjohn000@gmail.com>
2019-02-11 17:29:51 +0800

    Added sizecommand

--- src/seedu/addressbook/commands/SizeCommand.java
+++ src/seedu/addressbook/commands/SizeCommand.java
@@ -1,4 +1,20 @@
 package seedu.addressbook.commands;
 
-public class SizeCommand {
+/**
+ * Return the size of the address book
+ */
+public class SizeCommand extends Command {
+
+	public static final String COMMAND_WORD = "size";
+	public static final String MESSAGE_USAGE = COMMAND_WORD + ": Outputs the size of the address book "
+			+ "Example: " + COMMAND_WORD;
+
+	public static final String MESSAGE_SUCCESS = "Address book contains %d person(s).";
+
+	@Override
+	public CommandResult execute() {
+
+		int size = addressBook.size();
+		return new CommandResult(String.format(MESSAGE_SUCCESS, size));
+	}
 }
